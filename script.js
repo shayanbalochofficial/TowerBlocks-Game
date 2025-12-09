9b2881d4ec9dd6e9b54a312e56373f6d68449bf2
Shayan Baloch <shayanbalochofficial@gmail.com>
2025-12-09 22:12:05 +0500

    Doing JS

--- script.js
+++ script.js
@@ -67,3 +67,76 @@ var Stage = /** @class */ (function () {
   };
   return Stage;
 })();
+
+var Block = /** @class */ function () {
+  function Block(block) {
+    this.STATES = { ACTIVE: "active", STOPPED: "stopped", MISSED: "missed" };
+    this.MOVE_AMOUNT = 12;
+    this.dimension = { width: 0, height: 0, depth: 0 };
+    this.position = { x: 0, y: 0, z: 0 };
+    this.targetBlock = block;
+    this.index = (this.targetBlock ? this.targetBlock.index : 0) + 1;
+    this.workingPlane = this.index % 2 ? "x" : "z";
+    this.workingDimension = this.index % 2 ? "width" : "depth";
+    this.dimension.width = this.targetBlock
+      ? this.targetBlock.dimension.width
+      : 10;
+    this.dimension.height = this.targetBlock
+      ? this.targetBlock.dimension.height
+      : 2;
+    this.dimension.depth = this.targetBlock
+      ? this.targetBlock.dimension.depth
+      : 10;
+    this.position.x = this.targetBlock ? this.targetBlock.position.x : 0;
+    this.position.y = this.dimension.height * this.index;
+    this.position.z = this.targetBlock ? this.targetBlock.position.z : 0;
+    this.colorOffset = this.targetBlock
+      ? this.targetBlock.colorOffset
+      : Math.round(Math.random() * 100);
+    if (!this.targetBlock) {
+      this.color = 0x333344;
+    } else {
+      var offset = this.index + this.colorOffset;
+      var r = Math.sin(0.3 * offset) * 55 + 200;
+      var g = Math.sin(0.3 * offset + 2) * 55 + 200;
+      var b = Math.sin(0.3 * offset + 4) * 55 + 200;
+      this.color = new THREE.Color(r / 255, g / 255, b / 255);
+    }
+
+    this.state = this.index > 1 ? this.STATES.ACTIVE : this.STATES.STOPPED;
+
+    this.speed = -0.1 - this.index * 0.005;
+    if (this.speed < -4) this.speed = -4;
+    this.direction = this.speed;
+
+    var geometry = new THREE.BoxGeometry(
+      this.dimension.width,
+      this.dimension.height,
+      this.dimension.depth
+    );
+    geometry.applyMatrix(
+      new THREE.Matrix4().makeTranslation(
+        this.dimension.width / 2,
+        this.dimension.height / 2,
+        this.dimension.depth / 2
+      )
+    );
+    this.material = new THREE.MeshToonMaterial({
+      color: this.color,
+      shading: THREE.FlatShading,
+    });
+    this.mesh = new THREE.Mesh(geometry, this.material);
+    this.mesh.position.set(
+      this.position.x,
+      this.position.y + (this.state == this.STATES.ACTIVE ? 0 : 0),
+      this.position.z
+    );
+    if (this.state == this.STATES.ACTIVE) {
+      this.position[this.workingPlane] =
+        Math.random() > 0.5 ? -this.MOVE_AMOUNT : this.MOVE_AMOUNT;
+    }
+  }
+  Block.prototype.reverseDirection = function () {
+    this.direction = this.direction > 0 ? this.speed : Math.abs(this.speed);
+  };
+}();
