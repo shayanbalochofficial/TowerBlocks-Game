98d3504eff021427eeebb82ab16a521c1da8335f
Shayan Baloch <shayanbalochofficial@gmail.com>
2025-12-09 22:10:19 +0500

    Doing JS

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-console.clear();
 var Stage = /** @class */ (function () {
   function Stage() {
     var _this = this;
@@ -12,7 +11,6 @@ var Stage = /** @class */ (function () {
       this.scene.remove(elem);
     };
     this.container = document.getElementById("game");
-
     this.renderer = new THREE.WebGLRenderer({
       antialias: true,
       alpha: false,
@@ -37,5 +35,35 @@ var Stage = /** @class */ (function () {
     this.camera.position.y = 2;
     this.camera.position.z = 2;
     this.camera.lookAt(new THREE.Vector3(0, 0, 0));
+
+    this.light = new THREE.DirectionalLight(0xffffff, 0.5);
+    this.light.position.set(0, 499, 0);
+    this.scene.add(this.light);
+    this.softLight = new THREE.AmbientLight(0xffffff, 0.4);
+    this.scene.add(this.softLight);
+    window.addEventListener("resize", function () {
+      return _this.onResize();
+    });
+    this.onResize();
   }
+  Stage.prototype.setCamera = function (y, speed) {
+    if (speed === void 0) {
+      speed = 0.3;
+    }
+    TweenLite.to(this.camera.position, speed, {
+      y: y + 4,
+      ease: Power1.easeInOut,
+    });
+    TweenLite.to(this.camera.lookAt, speed, { y: y, ease: Power1.easeInOut });
+  };
+  Stage.prototype.onResize = function () {
+    var viewSize = 30;
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.camera.left = window.innerWidth / -viewSize;
+    this.camera.right = window.innerWidth / viewSize;
+    this.camera.top = window.innerHeight / viewSize;
+    this.camera.bottom = window.innerHeight / -viewSize;
+    this.camera.updateProjectionMatrix();
+  };
+  return Stage;
 })();
